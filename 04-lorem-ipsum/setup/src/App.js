5ae36663ec23cd08229c177e9f497efb661f6225
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-13 00:12:29 +0530

    Lorem Ipsum generator slice logic

--- 04-lorem-ipsum/setup/src/App.js
+++ 04-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,67 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count,setCount]= useState(0)
+
+  const [Data,setData]= useState([])
+ 
+  function handleForm(){
+
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+
+  <main>
+ <div className='section-center'>
+  <h3>Lorem Ipsum Generator</h3>
+  <div className='lorem-form'>
+    <label name="paragraph">Paragraphs:</label>
+    <input 
+   
+    onChange={(e)=>{
+     setCount(e.target.value)
+      
+    }}
+    type='number'/>
+    <button className='btn'
+    onClick={()=>{
+      let amount = parseInt(count);
+      if(parseInt(count)<=0){
+       amount=1
+      }
+      if(parseInt(count)>8){
+       amount=8
+      }
+      const finalData= data.slice(0,amount)
+     
+      setData(finalData)
+
+    }}
+    >GENERATE</button>
+  </div>
+
+{
+  Data.map(data=>{
+    return(
+      <>
+      <div className='result'>
+    <p>{data}
+
+
+    </p>
+  </div>
+      </>
+    )
+    
+  })
+}
+
+ </div>
+
+  </main>
+  
+
+
+  
     )
 }
 
