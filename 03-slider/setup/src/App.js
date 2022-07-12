1802cc51fbaab41cb2f2229a30cffdaa6fee4b05
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-12 16:55:05 +0530

    Slider functionality

--- 03-slider/setup/src/App.js
+++ 03-slider/setup/src/App.js
@@ -2,8 +2,97 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import people from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index,setIndex]= useState(0)
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index]);
+  useEffect(()=>{
+    const interval= setInterval(()=>{
+setIndex(index+1)
+    },5000)
+    return () => {
+      clearInterval(interval);
+    };
+
+  },[index])
+  return (
+    <>
+    <div className='section'>
+      <div className='title'>
+
+        <h2>  <span>/</span> Reviews</h2>
+      </div>
+    
+        <div className='section-center'>
+          { 
+            data.map((person,personIndex)=>{
+              let position= 'nextSlide'
+              if(personIndex===index){
+               position = 'activeSlide'
+              }
+              if(personIndex===index-1 || (index === 0 && personIndex === people.length - 1)){
+                position= 'lastSlide'
+              }
+              
+
+
+              return (<>
+              <article className={position}>
+          <img className='person-img'  src={person.image}></img>
+            <div className='title'>
+              
+            <h4>  <span>{person.name}</span></h4></div>
+            <p className="title">{person.title}</p>
+              <p className="text">{person.quote}</p>
+              <FaQuoteRight className="icon" />
+          </article>
+              
+              </>)
+            })
+          }
+          
+          <button
+          onClick={()=>{
+          
+              setIndex(index-1)
+      
+          }}
+          className='prev'
+          
+          
+          >{<FiChevronLeft></FiChevronLeft>}</button>
+
+          <button className='next' 
+             onClick={()=>{
+        
+                setIndex(index+1)
+              
+              
+          
+  
+            }}
+          >{<FiChevronRight></FiChevronRight>}</button>
+
+           
+        </div>
+
+
+
+
+    
+    </div>
+
+    
+    </>
+  )
 }
 
 export default App;
---
+++ 2.Tabs/setup/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+import Navbuttons from './Navbuttons'
+import Jobs from './Jobs'
+import Data from './Data'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  const [jobs,setJobs]= useState(Data)
+  const [order,setOrder]= useState(0)
+  const value= jobs[order]
+  function handleOrder(ord){
+    setOrder(ord)
+
+  }
+
+ 
+  return(
+    <>
+    <div className='section'>
+     
+      <h2 className='title'>Experience</h2>
+      <div className='underline'></div>
+      <div className='jobs-center'>
+        <Navbuttons jobs={jobs} order={order} handleOrder={handleOrder}> </Navbuttons>
+        <Jobs values={value}></Jobs>
+
+         
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
