3cc557e09679b7eb15770b8df98dcde8cafe29c2
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-15 00:47:55 +0530

    List component

--- 05-Grocery Bud/setup/src/List.js
+++ 05-Grocery Bud/setup/src/List.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items,handleTrash,handleEdit}) => {
+
+
+  return (
+    items.map(item=>{
+      
+          return(
+            <div className="grocery-item">
+             <span>{item.name}</span>
+             <div>
+             <button className='edit-btn'
+             onClick={()=>
+            {
+              handleEdit(item.id)
+            }}
+             ><FaEdit></FaEdit>  </button>
+             <button className='delete-btn'
+             onClick={()=>{
+              handleTrash(item.id)
+             }}
+             ><FaTrash></FaTrash></button>
+             </div>
+             
+            </div>
+          )
+    
+    })
+  )
+ 
 }
 
 export default List
