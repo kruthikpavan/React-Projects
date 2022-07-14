f02b9a5940f0bb8828f0c47ed82bd45bcae8f9e1
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-15 00:47:26 +0530

    Alert component

--- 05-Grocery Bud/setup/src/Alert.js
+++ 05-Grocery Bud/setup/src/Alert.js
@@ -1,7 +1,45 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({alertMessage,handleShow}) => {
+useEffect(()=>{
+  const timeOut= setTimeout(()=>{
+handleShow(false)
+  },3000)
+
+  return ()=> {
+    clearTimeout(timeOut)
+
+
+  }
+},[])
+  return(
+   
+    <div className="alert">
+        { alertMessage==="Item Added" && 
+        
+        <h5 className='alert-success'>Item Added</h5>}   
+
+        { alertMessage==="Item Deleted" && 
+        
+        <h5 className='alert-danger'>Item Deleted</h5>}
+
+{ alertMessage==="Items Cleared" && 
+        
+        <h5 className='alert-danger'>Items Cleared</h5>}
+
+{ alertMessage==="Item Edited" && 
+        
+        <h5 className='alert-success'>Item Edited</h5>}
+      
+    </div>
+  )
+  
+
+
+  
+    
+  
+  
 }
 
 export default Alert
