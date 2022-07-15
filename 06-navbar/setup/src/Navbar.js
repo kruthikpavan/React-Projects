b5a5d1cecc9348b152816022f5861dda712f26da
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-15 16:07:24 +0530

    Responsive Navbar Implemented

--- 06-navbar/setup/src/Navbar.js
+++ 06-navbar/setup/src/Navbar.js
@@ -4,7 +4,56 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+  const [show,setShow]= useState()
+ 
+
+  
+  return(
+    <>
+    <nav  className='nav-center'>
+      <div className="nav-header">
+        <h2>Pavan Kruthik</h2>
+        <div className="nav-toggle"
+        onClick={()=>{
+          show==='show-container'?setShow(''):setShow("show-container")
+   
+
+        }}>
+          <FaBars></FaBars>
+          
+        </div>
+      </div>
+<div className={`links-container ${show}`}>
+      <ul className="links">
+          {
+            links.map(link=>{
+              return <l>
+                <a >{link.text}</a>
+                </l>
+            })
+          }
+
+
+      </ul>
+     </div>
+     <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      
+     </nav>
+
+     
+    
+    </>
+  )
 }
 
 export default Navbar
